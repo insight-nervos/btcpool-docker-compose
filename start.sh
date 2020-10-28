f040d16aee0eb26285d889404286376d9d6f38b8
Pranav Thiruanvukkarasu <p_thiruanvukkarasu@u.pacific.edu>
2020-10-27 20:10:09 -0700

    Move btcpool config file to this repo

--- start.sh
+++ start.sh
@@ -1,14 +1,5 @@
-# clone repo
-git clone https://github.com/insight-stratum/btcpool.git
-
-# build base image
-sudo docker build -t btccom/btcpool_build:btc-0.16.3 -f ./btcpool/docker/btcpool/base-image/Dockerfile.btc --build-arg BUILD_JOBS=$(nproc) ./btcpool/docker/btcpool/base-image/
-
-# build deploy image
-sudo docker build -t btccom/btcpool-btc -f ./btcpool/docker/btcpool/deploy/Dockerfile --build-arg BASE_IMAGE=btccom/btcpool_build:btc-0.16.3 --build-arg BUILD_JOBS=$(nproc) --build-arg GIT_DESCRIBE=$(git describe --tag --long) ./btcpool/
-
 # move configs
-cp ./btcpool/src/bitcoin/cfg/* ./volumes/btcpool/
+cp ./config/btcpool/* ./volumes/btcpool/
 
 # start docker compose
 sudo docker-compose up
